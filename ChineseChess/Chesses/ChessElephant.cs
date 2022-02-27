4832ea6a8c5d0b3b34d627dc338abb26d87baf85
Zqzqqqq <691992215@qq.com>
2022-02-27 23:29:23 +0800

    zq commit1

--- ChineseChess/Chesses/ChessElephant.cs
+++ ChineseChess/Chesses/ChessElephant.cs
@@ -20,9 +20,10 @@ namespace ChineseChess.Chesses
                 if (row != 5 || row != 7 || row != 9)
                     return false;
                 if (col != 0 || col != 2 || col != 4 || col != 6 || col != 8)
-                    return true;
+                    return false;
                 bool flag = false;
                 ChessInfoArgument e = null;
+                
                 if (row == this.row - 2 && col == this.col - 2) // 左上
                 {
                     foreach(Chess c in chesses)
@@ -54,17 +55,92 @@ namespace ChineseChess.Chesses
                 }
                 if(row == this.row + 2 && col == this.col - 2) // 左下
                 {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row + 1 && c.col == this.col - 1) // 被卡象脚就不能移动
+                            return false;
+                        if (c.row == this.row + 2 && c.col == this.col - 2) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
 
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
                 }
                 if (row == this.row - 2 && col == this.col + 2) // 右上
                 {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row - 1 && c.col == this.col + 1) // 被卡象脚就不能移动
+                            return false;
+                        if (c.row == this.row - 2 && c.col == this.col + 2) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
 
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
                 }
                 if (row == this.row + 2 && col == this.col + 2) // 右下
                 {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row + 1 && c.col == this.col + 1) // 被卡象脚就不能移动
+                            return false;
+                        if (c.row == this.row + 2 && c.col == this.col + 2) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
 
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
                 }
-
+                return false;
             }
             else
             {
