105ceaf218dfbcd79db1f515bf90a21e256fad53
Zqzqqqq <691992215@qq.com>
2022-02-28 11:13:21 +0800

    zhouqi commit 2 ChessMove Completed

--- ChineseChess/Chesses/ChessMandarin.cs
+++ ChineseChess/Chesses/ChessMandarin.cs
@@ -14,7 +14,123 @@ namespace ChineseChess.Chesses
         }
         public override bool Move(int row, int col, List<Chess> chesses)
         {
-            throw new NotImplementedException();
+            bool b = this.row / 5 == 1;
+            if (row != 7 && row != 8 && row != 9 && col != 3 && col != 4 && col != 5)
+            {
+                return false;
+            }
+            else
+            {
+                bool flag = false;
+                ChessInfoArgument e = null;
+                if (row == this.row - 1 && col == this.col - 1) // 左上
+                {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row - 1 && c.col == this.col - 1) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
+                }
+                if (row == this.row + 1 && col == this.col - 1) // 左下
+                {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row + 1 && c.col == this.col - 1) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
+
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
+                }
+                if (row == this.row - 1 && col == this.col + 1) // 右上
+                {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row - 1 && c.col == this.col + 1) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
+
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
+                }
+                if (row == this.row + 1 && col == this.col + 1) // 右下
+                {
+                    foreach (Chess c in chesses)
+                    {
+                        if (c.row == this.row + 1 && c.col == this.col + 1) // 目标点有棋子
+                        {
+                            if (c.flag == this.flag)
+                                return false;
+                            flag = true;
+                            e = new ChessInfoArgument(c);
+                        }
+                    }
+                    if (flag)
+                    {
+                        this.row = row;
+                        this.col = col;
+                        base.OnEating(e);
+                        return true;
+                    }
+                    else
+                    {
+                        this.row = row;
+                        this.col = col;
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
     }
 }
